cb8fbb590d28ae0390e52d8720b5e8a3e5dcfd66
Hsinhao <hsinaohuang@gmail.com>
2024-03-30 15:29:24 +0800

    Add a script to load and save table text file

---
+++ Analysis/load_and_save_table.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def load_from_table( Filename_table: str ) -> tuple[float, float]:
+    """
+    This funcion load a two-column text file and return the two colums as two arrays.
+
+    Parameters:
+        Filename_table  : the filename of the input text file
+
+    Return:
+        Table_x         : the array for the fisrt column
+        Table_y         : the array for the second colum
+    """
+
+    # load the table
+    Table_x, Table_y = np.loadtxt( Filename_table, skiprows=1, unpack=True )
+
+    return Table_x, Table_y
+
+
+def save_table_to_file( Filename_output: str, Table_x: float, Table_y: float ) -> None:
+    """
+    This function save two arrays and save them into two columns in a text file.
+
+    Parameters:
+        Filename_output : the filename of the output text file
+        Table_x         : the array for the fisrt column
+        Table_y         : the array for the second colum
+
+    Return:
+        None
+    """
+
+    # save the table to text file
+    np.savetxt( Filename_output,
+                np.column_stack( (Table_x, Table_y) ),
+                fmt = '          %9.8e',
+                header = '         x (unit_of_x)            y (unit_of_y)' )
+
+    return
+
+def main() -> None:
+    """
+    This function is a example to read two columns from the text file and save them into another text file.
+
+    Parameters:
+        None
+
+    Return:
+        None
+    """
+
+    # readi a density profile
+    radius, density = load_from_table( "Density_table_input" )
+
+    # save the density profile to file
+    save_table_to_file( "Density_table_output", radius, density )
+
+    return
+
+if __name__ == '__main__':
+    main()
