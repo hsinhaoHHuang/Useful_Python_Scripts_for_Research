822f307ab0ef179608ea08e9a22efd8e2f5ac7be
Hsinhao Huang <hsinaohuang@gmail.com>
2024-04-03 15:26:58 +0800

    Update the script

--- Visualization/plot_standard_paper_figure_2D_phase_plot.py
+++ Visualization/plot_standard_paper_figure_2D_phase_plot.py
@@ -49,42 +49,41 @@ plt.rcParams['mathtext.bf']       = 'STIXGeneral:italic:bold'
 ###################################################################################################
 # Setting for Plotting
 ###################################################################################################
-n_rows             = 2                 # number of rows in the figure panels
-n_cols             = 3                 # number of columns in the figure panels
+n_rows             = 2                     # number of rows in the figure panels
+n_cols             = 3                     # number of columns in the figure panels
 
-# fields to plot the projections
-field_toplot       = ('gas','cell_mass') # target field
-field_x            = ('gas','density')
-field_y            = ('gas','temperature')
+# fields to plot the phase plots
+field_z            = ('gas','cell_mass')   # target field
+field_x            = ('gas','density')     # field at the x-axis
+field_y            = ('gas','temperature') # field at the y-axis
 
-Plotting_UNIT_L    = 'kpc'             # length unit in the plot
-Plotting_UNIT_D    = 'Msun/kpc**3'     # density unit in the plot
-Plotting_UNIT_M    = 'Msun'            # density unit in the plot
-Plotting_UNIT_Temp = 'K'               # time unit in the plot
+Plotting_UNIT_L    = 'kpc'                 # length unit in the plot
+Plotting_UNIT_D    = 'Msun/kpc**3'         # density unit in the plot
+Plotting_UNIT_M    = 'Msun'                # density unit in the plot
+Plotting_UNIT_Temp = 'K'                   # time unit in the plot
 
 UNIT_D_todisplay   = r'$\mathit{\rho}\ ({\rm M}_{\odot}{\rm kpc}^{\rm -3})$'
 UNIT_M_todisplay   = r'$\mathit{M}\ ({\rm M}_{\odot})$'
 UNIT_Temp_todisplay= r'$\mathit{T}\ ({\rm K})$'
 
-zoom               = 30                # region for main plot
+sphere_radius      = -1.0                  # radius of sphere for the phase plots (in plotting unit)
 
-slab_thickness     = -1.0              # slab thickness for the projection (in plotting unit)
+colormap           = 'viridis'             # ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
+color_lim_min      = 1.e0                  # color bar lower limit for z-field (in plotting unit)
+color_lim_max      = 1.e7                  # color bar upper limit for z-field (in plotting unit)
 
-colormap           = 'viridis'         # ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
-color_lim_min      = 1.e0              # color bar upper limit for projection (in plotting unit)
-color_lim_max      = 1.e7              # color bar lower limit for projection (in plotting unit)
-x_lim_min          = 1.e2
-x_lim_max          = 1.e8
-y_lim_min          = 1.e3
-y_lim_max          = 1.e6
+x_lim_min          = 1.e2                  # lower limit for the x-field 
+x_lim_max          = 1.e8                  # upper limit for the x-field
+y_lim_min          = 1.e3                  # lower limit for the y-field
+y_lim_max          = 1.e6                  # upper limit for the y-field
 
-grid_rect_left     =  0.04             # left boundary of the figure panels
-grid_rect_bottom   =  0.08             # bottom boundary of the figure panels
-grid_rect_width    =  0.90             # width of the figure panels
-grid_rect_height   =  0.90             # height of the figure panels
+grid_rect_left     = 0.04                  # left boundary of the figure panels
+grid_rect_bottom   = 0.08                  # bottom boundary of the figure panels
+grid_rect_width    = 0.90                  # width of the figure panels
+grid_rect_height   = 0.90                  # height of the figure panels
 
-cm                 = 1/2.54            # centimeters in inches
-fig_size_x         = 16*cm             # output figure size in the horizontal direction
+cm                 = 1/2.54                # centimeters in inches
+fig_size_x         = 16*cm                 # output figure size in the horizontal direction
 fig_size_y         = (fig_size_x-1*cm)/n_cols*n_rows
 fig                = plt.figure( 1,(fig_size_x, fig_size_y), dpi=dpi )
 ###################################################################################################
@@ -122,20 +121,16 @@ def main() -> None:
 
         ###################################################################################################
         # Region to plot
-        Data_Center_x         = ds.domain_center[0].in_units('code_length').d
-        Data_Center_y         = ds.domain_center[1].in_units('code_length').d
-        Data_Center_z         = ds.domain_center[2].in_units('code_length').d
+        Data_Center_x         = ds.all_data().quantities.max_location('density')[1].in_units('code_length').d
+        Data_Center_y         = ds.all_data().quantities.max_location('density')[2].in_units('code_length').d
+        Data_Center_z         = ds.all_data().quantities.max_location('density')[3].in_units('code_length').d
         Data_Center           = ds.arr( [Data_Center_x, Data_Center_y, Data_Center_z], 'code_length' )
-        Data_Width            = ds.domain_width[0]/zoom
-        Data_slab_thickness   = ds.quan( slab_thickness , Plotting_UNIT_L ) if slab_thickness > 0.0 else Data_Width
-        Data_loc              = Data_Center
-        Data_corner_L         = Data_loc - ds.arr( [0.5*Data_Width, 0.5*Data_Width, 0.5*Data_slab_thickness] )
-        Data_corner_R         = Data_loc + ds.arr( [0.5*Data_Width, 0.5*Data_Width, 0.5*Data_slab_thickness] )
-        Data_region           = ds.box( Data_corner_L, Data_corner_R )
+        Data_sphere_radius    = ds.quan( sphere_radius, Plotting_UNIT_L ) if sphere_radius > 0.0 else 0.4*ds.domain_width[0]
+        Data_region           = ds.sphere( Data_Center, Data_sphere_radius )
 
         ###################################################################################################
         # Phase plot
-        phas = yt.PhasePlot( Data_region, field_x, field_y, field_toplot,
+        phas = yt.PhasePlot( Data_region, field_x, field_y, field_z,
                                           weight_field=None, x_bins=300, y_bins=300 )
 
         # Set the font
@@ -143,17 +138,17 @@ def main() -> None:
 
 
         # Set the field unit
-        phas.set_unit( field_x,      Plotting_UNIT_D )
-        phas.set_unit( field_y,      Plotting_UNIT_Temp )
-        phas.set_unit( field_toplot, Plotting_UNIT_M )
+        phas.set_unit( field_x, Plotting_UNIT_D    )
+        phas.set_unit( field_y, Plotting_UNIT_Temp )
+        phas.set_unit( field_z, Plotting_UNIT_M    )
 
         # Set the colorbar limits
-        phas.set_xlim( x_lim_min, x_lim_max )
-        phas.set_ylim( y_lim_min, y_lim_max )
-        phas.set_zlim( field_toplot, color_lim_min, color_lim_max )
+        phas.set_xlim(              x_lim_min,     x_lim_max )
+        phas.set_ylim(              y_lim_min,     y_lim_max )
+        phas.set_zlim( field_z, color_lim_min, color_lim_max )
 
         # Set the colormap
-        phas.set_cmap( field_toplot, colormap )
+        phas.set_cmap( field_z, colormap )
 
         # Set the x label
         phas.set_xlabel( UNIT_D_todisplay )
@@ -162,35 +157,34 @@ def main() -> None:
         phas.set_ylabel( UNIT_Temp_todisplay )
 
         # Set the colorbar label
-        phas.set_colorbar_label( field_toplot, UNIT_M_todisplay )
+        phas.set_colorbar_label( field_z, UNIT_M_todisplay )
 
 
         # Annotate the text for title
-        #phas.annotate_text( xpos=0.05, ypos=0.90, text='Data %d'%(panel_idx) ) #,
-                          #  { "size":1.25*FONT_SIZE, "color":"white", "weight":"normal", "bbox":dict(boxstyle="round",ec='white',fc='white',alpha=0.0) } )
+        phas.annotate_text( xpos=1.2*x_lim_min, ypos=0.80*y_lim_max, text='Data %d'%(panel_idx),
+                            size=1.25*FONT_SIZE, color="white", weight="normal", bbox=dict(boxstyle="round",ec='white',fc='white',alpha=0.0) } )
 
 
 
 
         ###################################################################################################
         # Set the plots to the grid
-        plot        = phas.plots[field_toplot]
+        plot        = phas.plots[field_z]
         plot.figure = fig
         plot.axes   = grid[panel_idx].axes
-        #if panel_idx == n_rows*n_cols-1:
-        #    plot.cax    = grid.cbar_axes[panel_idx]
         plot.cax    = grid.cbar_axes[0]
 
+        # actually render the phase plot
         phas.render()
 
         # Set the ticks
-
-        grid[panel_idx].axes.xaxis.get_ticklocs(minor=True)
-        grid[panel_idx].axes.yaxis.get_ticklocs(minor=True)
+        grid[panel_idx].axes.xaxis.get_ticklocs( minor=True )
+        grid[panel_idx].axes.yaxis.get_ticklocs( minor=True )
         grid[panel_idx].axes.minorticks_on()
         grid[panel_idx].axes.tick_params( which='both', left=True, right=True, bottom=True, top=True )
-        grid[panel_idx].tick_params(which='both', direction='in')
+        grid[panel_idx].tick_params( which='both', direction='in' )
 
+        # only the L-shape
         if ( panel_idx%n_cols == 0 ):
             grid[panel_idx].axes.tick_params( labelleft=True, labelright=False )
         else:
@@ -200,6 +194,10 @@ def main() -> None:
             grid[panel_idx].axes.tick_params( labelbottom=True, labeltop=False )
         else:
             grid[panel_idx].axes.tick_params( labelbottom=False, labeltop=False )
+
+        # Annotate the text for title
+        #grid[panel_idx].axes.annotate_text( x=0.05, y=0.90, 'Data %d'%(panel_idx), transform=grid[panel_idx].axes.transAxes,
+        #                                    size=1.25*FONT_SIZE, color="white", weight="normal", bbox=dict(boxstyle="round",ec='white',fc='white',alpha=0.0) } )
 
         ###################################################################################################
 
