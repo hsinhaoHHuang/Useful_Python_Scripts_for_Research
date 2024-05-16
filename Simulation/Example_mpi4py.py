262096657c8dafa132875bb204b618a7e415dc14
Hsinhao <hsinaohuang@gmail.com>
2024-05-16 21:13:24 +0800

    Minor adjustments

--- Simulation/Example_mpi4py.py
+++ Simulation/Example_mpi4py.py
@@ -41,11 +41,13 @@ def main() -> None:
 
     data_sending    = my_rank*np.ones( numData_sending )
     comm.Send( data_sending, dest=next_rank )
+
     print( f'Rank-{my_rank:02d} data sent:     ', data_sending )
 
     numData_recving = comm.recv( source=prev_rank )
     data_recving    = np.empty( numData_recving, dtype='d' )  # allocate space to receive the array
     comm.Recv( data_recving, source=prev_rank )
+
     print( f'Rank-{my_rank:02d} data received: ', data_recving )
 
     comm.Barrier()
@@ -55,10 +57,8 @@ def main() -> None:
     if my_rank == 0:
         print( '' )
         print( 'Bcast' )
+
     if my_rank == 0:
-        # create a data array on process 0
-        # in real code, this section might
-        # read in data parameters from a file
         numData_bcasting = 10
         data_bcasting    = np.linspace( 0.0, 0.9, numData_bcasting )
         print( f'Rank-{my_rank:02d} data bcasting: ', data_bcasting )
@@ -88,13 +88,13 @@ def main() -> None:
     if my_rank == 0:
         data_scattering = np.linspace( 1, Nrank*numDataPerRank_scattering, Nrank*numDataPerRank_scattering )
         print( f'Rank-{my_rank:02d} data scattering: ', data_scattering )
-        # when Nrank=4 (using -n 4), data = [1.0:40.0]
     else:
         data_scattering = None
 
     data_scattered = np.empty( numDataPerRank_scattering, dtype='d' ) # allocate space
 
     comm.Scatter( data_scattering, data_scattered, root=0 )
+
     print( f'Rank-{my_rank:02d} data scattered:  ', data_scattered )
 
     comm.Barrier()
@@ -104,11 +104,14 @@ def main() -> None:
     if my_rank == 0:
         print( '' )
         print( 'Gather' )
+
     numDataPerRank_gathering = 10
     data_gathering = np.linspace( my_rank*numDataPerRank_gathering+1, (my_rank+1)*numDataPerRank_gathering, numDataPerRank_gathering )
+
     print( f'Rank-{my_rank:02d} data gathering: ', data_gathering )
 
     data_gathered = None
+
     if my_rank == 0:
         data_gathered = np.empty( numDataPerRank_gathering*Nrank, dtype='d' )
 
@@ -129,11 +132,11 @@ def main() -> None:
 
     print( f'Rank-{my_rank:02d} data_reducing = ', data_reducing )
 
-    # initialize the np arrays that will store the results:
+    # initialize the np arrays that will store the results
     data_sum = np.array( 0.0, 'd' )
     data_max = np.array( 0.0, 'd' )
 
-    # perform the reductions:
+    # perform the reductions
     comm.Reduce( data_reducing, data_sum, op=MPI.SUM, root=0 )
     comm.Reduce( data_reducing, data_max, op=MPI.MAX, root=0 )
 
