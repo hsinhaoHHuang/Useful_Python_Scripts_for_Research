4a8d20f78e9285446c446f0e509709da50afe225
Hsinhao <hsinaohuang@gmail.com>
2024-06-03 19:25:22 +0800

    Update to use consistent format

--- Simulation/Check_hdf5_data.py
+++ Simulation/Check_hdf5_data.py
@@ -1,40 +1,59 @@
+#!/usr/bin/env python3
+
 import h5py
 import numpy as np
 
 
-# input filename
-f = h5py.File( 'Data_000000', 'r' )
+def main() -> None:
+    """main function
+    This function read and print out a hdf5 data.
 
+    Parameters
+    ----------
+    None
 
-for group in list(f):
-    print( '- Group ', group )
+    Returns
+    -------
+    None
 
-    for dset in list(f[group]):
-        print( '           - dset ', dset )
+    """
 
-        for attribute in list( f[group][dset].attrs ):
-            print( '                     - attribute ', attribute, '=', f[group][dset].attrs[attribute] )
+    # input filename
+    f = h5py.File( 'Data_000000', 'r' )
 
-        if group == 'GridData':
-            GID = 0
-            k   = 0
-            j   = 0
-            i   = 0
-            print( '                     - size   = ', len( list( f[group][dset] ) ) )
-            print( '                     - value  = ', f[group][dset][GID][k][j][i], ' at GID =', GID, 'k =', k, 'j =', j, 'i =', i )
+    # output the data structure
+    for group in list(f):
+        print( '- Group ', group )
 
-        elif group == 'Particle':
-            print( '                     - size   = ', len( list( f[group][dset] ) ) )
-            print( '                     - value  = ', f[group][dset][0:10].tolist() )
+        for dset in list(f[group]):
+            print( '           - dset ', dset )
 
-        elif group == 'Info':
-            for name in list(f[group][dset].dtype.names):
-               print( '                     - %-30s ='%name, np.array( f[group][dset][name] ).tolist() )
+            for attribute in list( f[group][dset].attrs ):
+                print( '                     - attribute ', attribute, '=', f[group][dset].attrs[attribute] )
 
-        elif group == 'Tree':
-            print( '                     - shape    =', f[group][dset].shape       )
-            print( '                     - value[0] =', f[group][dset][0].tolist() )
+            if group == 'GridData':
+                GID = 0
+                k   = 0
+                j   = 0
+                i   = 0
+                print( '                     - size   = ', len( list( f[group][dset] ) ) )
+                print( '                     - value  = ', f[group][dset][GID][k][j][i], ' at GID =', GID, 'k =', k, 'j =', j, 'i =', i )
+
+            elif group == 'Particle':
+                print( '                     - size   = ', len( list( f[group][dset] ) ) )
+                print( '                     - value  = ', f[group][dset][0:10].tolist() )
+
+            elif group == 'Info':
+                for name in list(f[group][dset].dtype.names):
+                   print( '                     - %-30s ='%name, np.array( f[group][dset][name] ).tolist() )
+
+            elif group == 'Tree':
+                print( '                     - shape    =', f[group][dset].shape       )
+                print( '                     - value[0] =', f[group][dset][0].tolist() )
+
+            print( '' )
 
         print( '' )
 
-    print( '' )
+if __name__ == '__main__':
+    main()
